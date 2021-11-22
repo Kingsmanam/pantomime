382576536fee2f808f2d0cb9605b36c5ea0f1899
Sina <43543230+sssinaaa@users.noreply.github.com>
2021-11-22 16:11:37 +0330

    global

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import React from "react";
 import RollDice from "./RollDice";
+import "./App.css";
 
-function Item() {
-  return <div className="divsub">{data1[0]}</div>;
-}
-
-const data1 = ["salam", "bye"];
 function App() {
+  const items = [
+    "very easy",
+    "easy",
+    "normal",
+    "intermediate",
+    "hard",
+    "very hard",
+  ];
+
   return (
-    <div className="div">
-      <RollDice />
-      <button
-        onClick={() => {
-          <Item />;
-        }}
-        className="div1"
-      >
-        <p className="p1">-1</p>
-      </button>
-      <button className="div1">
-        <p className="p1">-2</p>
-      </button>
-      <button className="div1">
-        <p className="p1">-2</p>
-      </button>
-      <button className="div1">
-        <p className="p1">-2</p>
-      </button>
-      <button className="div1">
-        <p className="p1">-2</p>
-      </button>
-      <button className="div1">
-        <p className="p1">-2</p>
-      </button>
+    <div className="container">
+      <div>
+        <RollDice />
+      </div>
+
+      <div className="items">
+        {items.map((item) => {
+          return <div className="item">{item}</div>;
+        })}
+      </div>
     </div>
   );
 }
